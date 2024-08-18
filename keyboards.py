69b4e25b937221ed91acee8d034b797fec9f82e3
Yevhenii Baranov <j52b10i78@gmail.com>
2024-08-18 17:48:05 +0200

    bug finded

--- keyboards.py
+++ keyboards.py
@@ -6,16 +6,15 @@ async def get_categories_kb():
         [InlineKeyboardButton(text='Musicians', callback_data='cat:musicians')],
         [InlineKeyboardButton(text='Entertainment services', callback_data='cat:services')]
     ]
-    kb = InlineKeyboardMarkup(
-        inline_keyboard=buttons
-    )
+    kb = InlineKeyboardMarkup(inline_keyboard=buttons)
     return kb
 
 async def get_subcategory_kb(link: str):
+    b_data = f'sub:{link}'
+    if len(b_data) > 64:
+        raise ValueError("Callback data is too long")
     button = [
-        [InlineKeyboardButton(text='Choose', callback_data=f'sub:{link}')]
+        [InlineKeyboardButton(text='Choose', callback_data=b_data)]
     ]
-    kb = InlineKeyboardMarkup(
-        inline_keyboard=button
-    )
+    kb = InlineKeyboardMarkup(inline_keyboard=button)
     return kb
